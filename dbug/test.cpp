c520040a3f2a5ff5214c3a1d438e975a7da3db55
Alexander <Alexander@ALEXANDER-PC>
2017-04-11 23:59:54 +0200

    Implement test_string members

--- dbug/test.cpp
+++ dbug/test.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
-#include "../ltcl/string.h"
+#include "../test/test_string.h"
 
 #define WRITE(x) std::cout << x << std::endl
 
-template<class C, class A>
-void print_string(const ltc::String<C,A>& str) {
-	for (auto x : str) {
-		std::cout << x;
-	}
-	std::cout << std::endl;
-}
 
 int main() {
-	ltc::String<char> str{"hi"};
-	print_string(str);
+	try {
+		ltc::test::test_string::size();
+		ltc::test::test_string::length();
+		ltc::test::test_string::capacity();
+		ltc::test::test_string::push_back();
+		ltc::test::test_string::pop_back();
+		ltc::test::test_string::c_str();
+		ltc::test::test_string::append();	
+	}
+	catch (error& err) {
+		WRITE(err.what());
+	}
 }
